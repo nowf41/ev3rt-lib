c33a68e0ef57f383d9c6327e4aad91d556875ddc
nowf41 <wheat.komugi41@hotmail.com>
2025-12-17 21:04:39 +0900

    REF made utils a header-only library

--- utils.h
+++ utils.h
@@ -1,24 +1,44 @@
 #ifndef _UTILS_H_
 #define _UTILS_H_
 
+#include <cassert>
+
 namespace utils {
     template <typename T>
-    T clamp(T v, T low, T high);
+    T clamp(T v, T low, T high) {
+        if (v < low) {
+            return low;
+        } else if (high < v) {
+            return high;
+        } else {
+            return v;
+        }
+    }
 
     template <typename T>
-    T abs(T val);
+    T abs(T val) {
+        if (val >= static_cast<T>(0)) {
+            return val;
+        } else {
+            return -val;
+        }
+    }
 
     template <typename T>
     class Ema {
     private:
-        T val;
         T ret;
+        T val;
 
     public:
-        Ema();
-        Ema(T ret, T val);
-        void add(T value);
-        T get();
+        Ema() {}
+        Ema(T ret, T val) : ret(ret), val(val) {}
+        void add(T value) {
+            this->val = this->val*(this->ret) + value*(static_cast<T>(1)-(this->ret));
+        }
+        T get() {
+            return this->val;
+        }
     };
 }
 
